31a60e68a2782f1fce761970e342bc91b49f92ba
Drago96 <dragproychev@gmail.com>
2018-01-22 17:45:56 +0200

    Initial back end controllers and view models implementation

---
+++ TestMakerFreeWebApp/Controllers/QuizController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TestMakerFreeWebApp.ViewModels;
+
+namespace TestMakerFreeWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class QuizController : Controller
+    {
+        #region RESTful conventions methods
+        /// <summary>
+        /// GET: api/quiz/{id}
+        /// Retrieves the Quiz with the given {id}
+        /// </summary>
+        /// <param name="id">The ID of an existing Quiz</param>
+        /// <returns>the Quiz with the given {id}</returns>
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            // create a sample quiz to match the given request
+            var v = new QuizViewModel()
+            {
+                Id = id,
+                Title = String.Format("Sample quiz with id {0}", id),
+                Description = "Not a real quiz: it's just a sample!",
+                CreatedDate = DateTime.Now,
+                LastModifiedDate = DateTime.Now
+            };
+            // output the result in JSON format
+            return new JsonResult(
+                v,
+                new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
+        }
+
+        /// <summary>
+        /// Adds a new Quiz to the Database
+        /// </summary>
+        /// <param name="m">The QuizViewModel containing the data to insert</param>
+        [HttpPost]
+        public IActionResult Post(QuizViewModel m)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Edit the Quiz with the given {id}
+        /// </summary>
+        /// <param name="m">The QuizViewModel containing the data to update</param>
+        [HttpPut]
+        public IActionResult Put(QuizViewModel m)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Deletes the Quiz with the given {id} from the Database
+        /// </summary>
+        /// <param name="id">The ID of an existing Quiz</param>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        // GET: api/latest
+        #region Attribute-based routing methods
+        /// <summary>
+        /// GET: api/quiz/latest
+        /// Retrieves the {num} latest Quizzes
+        /// </summary>
+        /// <param name="num">the number of quizzes to retrieve</param>
+        /// <returns>the {num} latest Quizzes</returns>
+        [HttpGet("latest/{num}")]
+        public IActionResult Latest(int num = 10)
+        {
+            var sampleQuizes = new List<QuizViewModel>();
+
+            // add a first sample quiz
+            sampleQuizes.Add(new QuizViewModel()
+            {
+                Id = 1,
+                Title = "Which Shingeki No Kyojin character are you?",
+                Description = "Anime-related personality test",
+                CreatedDate = DateTime.UtcNow,
+                LastModifiedDate = DateTime.UtcNow
+            });
+
+            // add a bunch of other sample quizzes
+            for (int i = 2; i <= num; i++)
+            {
+                sampleQuizes.Add(new QuizViewModel()
+                {
+                    Id = i,
+                    Title = String.Format("Sample Quiz {0}", i),
+                    Description = "This is a sample quiz",
+                    CreatedDate = DateTime.Now,
+                    LastModifiedDate = DateTime.Now
+                });
+            }
+
+            // output the result in JSON format
+            return new JsonResult(
+                sampleQuizes,
+                new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
+        }
+        #endregion
+
+        /// <summary>
+        /// GET: api/quiz/bytitle
+        /// Retrieves the {num} Quizzes sorted by title {A to Z}
+        /// </summary>
+        /// <param name="num">the number of quizzes to retrieve</param>
+        /// <returns>{num} Quizzes sorted by title</returns>
+        [HttpGet("bytitle/{num:int?}")]
+        public IActionResult ByTitle(int num = 10)
+        {
+            var sampleQuizzes = ((JsonResult)this.Latest(num)).Value as List<QuizViewModel>;
+
+            return new JsonResult(
+                sampleQuizzes.OrderBy(t => t.Title),
+                new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
+        }
+
+        /// <summary>
+        /// GET: api/quiz/mostViewed
+        /// Retrieves the {num} random quizzes
+        /// <param name="num">the number of quizzes to retrieve</param>
+        /// <return>{num} random quizzes</return>
+        /// </summary>
+        [HttpGet("random/{num:int?}")]
+        public IActionResult Random(int num = 10)
+        {
+            var sampleQuizzes = ((JsonResult)Latest(num)).Value
+                as List<QuizViewModel>;
+
+            return new JsonResult(
+                sampleQuizzes.OrderBy(t => Guid.NewGuid()),
+                new JsonSerializerSettings()
+                {
+                    Formatting = Formatting.Indented
+                });
+        }
+
+    }
+}
